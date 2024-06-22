ba5c7f848eac0edc8463e53fa3121bb661439bbe
Sarah <sarah.emmoth@gmail.com>
2024-06-22 14:30:48 +0200

    removed unused fonts, replace pixel fony

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/*.{jsx,html}",
-    "index.html",
-    "./src/components/**/*.jsx"
-  ],
+  content: ["./src/**/*.{jsx,html}", "index.html"],
   theme: {
     extend: {
       fontFamily: {
-        bitmap: ['Bitmap', "mono-space"],
-        oxygen: ['Oxygen', "mono-space"],
-        hatton: ['Hatton', "mono-space"]
-
+        bitmap: ["Bitmap", "monospace"],
+        oxygen: ["Oxygen", "monospace"],
       },
     },
   },
+  plugins: [require("daisyui")],
   daisyui: {
     themes: [
-      "dracula",
-      "retro",
+      {
+        mainTheme: {
+          primary: "#5D3FD3",
+          secondary: "#a78bfa",
+          accent: "#4f46e5",
+          neutral: "#374151",
+          "base-100": "#1f2937",
+          info: "#7dd3fc",
+          success: "#22c55e",
+          warning: "#ef4444",
+          error: "#fde047",
+        },
+      },
     ],
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+};
